dbc15af0e9b3ac079810958cfd018d4e8da688cf
unknown <jo.cunha.esteves@gmail.com>
2015-05-20 17:25:32 +0100

    Added file to save references

--- src/cli/Main.java
+++ src/cli/Main.java
@@ -33,6 +33,6 @@ public class Main {
         System.out.println("Press Any Key To Continue...");
         new java.util.Scanner(System.in).nextLine();
 
-        // BBInstanceSolver.bbTree.display();
+         //BBInstanceSolver.bbTree.display();
     }
 }
